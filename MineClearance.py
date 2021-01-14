2b6d3d2dec9100837dc5e68235cf0bd93331efd4
zlx <zlx@163.com>
2021-01-14 18:10:07 +0800

    1.14end

--- MineClearance.py
+++ MineClearance.py
@@ -92,31 +92,42 @@ screen = pygame.display.set_mode((600,500))
 pygame.display.set_caption("Drop ball")
 color = 255,255,0
 rect_baffle_x,rect_baffle_y,rect_baffle_w,rect_baffle_h = 250,460,100,40
-circle_ball_x,circle_ball_y,circle_ball_r,circle_ball_w = random.randint(30,570),30,30,2
+circle_ball_x,circle_ball_y,circle_ball_r,circle_ball_w = random.randint(30,570),50,30,2
 rect_baffle = [rect_baffle_x,rect_baffle_y,rect_baffle_w,rect_baffle_h]
 circle_ball = [circle_ball_x,circle_ball_y,circle_ball_r,circle_ball_w]
 move_x,move_y,move_flag = 0,1,0
 move = [move_x,move_y,move_flag]
+live,score = 3,0
+information = [live,score]
 
-def print_text():
-    
-
-def ball_move(circle_ball,move):
+def print_all(information):
+    myfont = pygame.font.Font(None,20)
+    pygame.draw.line(screen,(255,255,255),(0,20),(600,20),2)
+    imagetext = myfont.render("Live:" + str(information[0]),True, (255,255,255))
+    screen.blit(imagetext,(0,0))
+    imagetext = myfont.render("Score:" + str(information[1]),True,(255,255,255))
+    screen.blit(imagetext,(550,0))
+def ball_move(circle_ball,move,information):
     circle_ball[1] += move[1]
     circle_ball[0] += move[0]
     if (circle_ball[1]+30) == 460 and circle_ball[0] >= rect_baffle[0] and circle_ball[0] <= (rect_baffle[0]+100):
         move[1] = -move[1]
+        information[1] += 1
         if move[2] == -1:
             move[0] += -1
         else:
             move[0] += 1
-    if (circle_ball[0]+30) >= 600 or (circle_ball[0]-30) <= 0:
+    if (circle_ball[0]+30) >= 600 or (circle_ball[0]-50) <= 0:
         move[0] = -move[0]
-    if (circle_ball[1]-30) <= 0:
+    if (circle_ball[1]-50) <= 0:
         move[1] = -move[1]
     if (circle_ball[1]+30) >= 500:
-        pygame.quit()
-        sys.exit()
+        if information[0] == 0:
+            pygame.quit()
+            sys.exit()
+        else:
+            information[0] -= 1
+            circle_ball[1] -= 420
 
 while True:
     for event in pygame.event.get():
@@ -133,8 +144,9 @@ while True:
                     rect_baffle[0] += 20
                 move[2] = 1
     screen.fill((0,0,0)) 
-    pygame.time.delay(10)
-    ball_move(circle_ball,move)
+    print_all(information)
+    pygame.time.delay(5)
+    ball_move(circle_ball,move,information)
     pygame.draw.rect(screen, color, (rect_baffle[0],rect_baffle[1],rect_baffle[2],rect_baffle[3]),2)
     pygame.draw.circle(screen, color,(circle_ball[0],circle_ball[1]),circle_ball[2],circle_ball[3]) 
     pygame.display.update()
